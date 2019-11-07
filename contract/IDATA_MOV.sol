a31ec7e1fea50d9cf60d15292e546cab2508104f
1999321 <43677014+1999321@users.noreply.github.com>
2019-11-07 22:37:37 +0800

    Create IDATA_MOV.sol

---
+++ contract/IDATA_MOV.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.5;
+interface IDATA_MOV{
+    function confirmed_Synchronize(address add,bool isconfirmed,uint256 id)external returns(bool);
+    function _transfer(uint256 value,address destination)external payable returns(bool);
+    function addTransaction(uint256 value,string calldata _Description,address destination,uint8 degree)external returns(uint256 id);
+    function ex_affair(uint transactionid)external returns(bool);
+    function Respond(uint requestid)external returns(bool);
+    function whitenames_add(address add)external returns(bool);
+    function whitenames_delete(address add)external returns(bool);
+    function divert(address add)external returns(bool);
+    function _open_to(address add,uint8 level)external ;
+    function Initiate_a_request(address organ_address,uint256 value,string calldata _Description,uint8 degree)external  returns(bool);
+    function request_id(uint256 id,uint256 id_request)external returns(bool);
+    function _transfer_Investors_or_client(uint256 value)external payable;
+    function get_transaction(uint id)external view returns (
+        uint8 status,  
+        uint8 required,
+        address destination,
+        address contract_address,
+        uint256 value,
+        uint256 start_time,
+        uint256 done_time,
+        string memory Description);
+   function get_transaction_confirmed(uint256 id,address add)external view returns(bool);
+   function get_request(uint id)external view returns(
+       address to,
+        uint256 value,
+        uint256 _id,
+        uint256 start_time,
+        uint256 done_time,
+        bool executed);
+    
+}
